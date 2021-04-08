664b0f61b584d0405f8f1acac3c82238c5d67be5
UH <554c46@gmail.com>
2021-04-08 12:34:34 +0200

    pkg version variable added

--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup
 import pypandoc
 
 
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='torch-tweaks',
-      version='0.1.0',
+      version=get_version("torch_tweaks/__init__.py"),
       description='Utility functions for PyTorch.',
       long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/torch-tweaks',
--- torch_tweaks/__init__.py
+++ torch_tweaks/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.1.1'
+
 from .idseqs_to_mask import idseqs_to_mask
