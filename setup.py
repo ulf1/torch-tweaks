833dc96627ff67bcefb3e4a5cbe6814a45e702b1
UH <554c46@gmail.com>
2021-04-08 11:19:50 +0200

    rst for pypi, zip file install

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='torch-tweaks',
       version='0.1.0',
       description='Utility functions for PyTorch.',
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/torch-tweaks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -20,4 +15,4 @@ setup(name='torch-tweaks',
           'setuptools>=40.0.0',
           'torch>=1.0.0'],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
